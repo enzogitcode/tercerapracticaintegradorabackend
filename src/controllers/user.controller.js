ab0aa2c9e6151b9b3c738c4dfac5619ec9128e6e
enzogit <enzo_91_10@hotmail.com>
2024-07-15 19:12:06 -0300

    emailcontroller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,11 +1,32 @@
 import UserModel from "../models/product.model";
 const newUser = new UserModel
+import EmailManager from "../services/email";
+const emailManager = new EmailManager()
 
 class UserController {
-    async register () {
+    async requestPasswordReset(req, res) {
+        const { email } = req.body
+        try {
+            const user = await UserModel.findOne({ email })
+            if (!user) {
+                return res.status(404).send("Usuario no encontrado")
 
+            }
+            const token = generarResetToken()
+            user.resetToken = {
+                token: token,
+                expire: new Date(Date.now() + 3600000)
+
+            }
+            await user.save()
+            await emailManager.sendRecoveryMail(email, user.first_name, token)
+            res.redirect("/confirmacion-envio")
+        } catch (error) {
+            res.status(500).send("Error interno del servidor")
+            
+        }
     }
-    
+
 }
 
 export default UserController
--- src/services/email.js
+++ src/services/email.js
@@ -6,19 +6,19 @@ class EmailManager {
             service: "gmail",
             port: 587,
             auth: {
-                user: ""
-                , pass: ""
+                user: "coderhouse50015@gmail.com"
+                , pass: "norp renb afxw uxyq"
 
             }
         })
     }
-    async enviarCorreoCompra(email, firt_name, ticket) {
+    async sendPurchaseMail(email, first_name, ticket) {
         try {
             const mailOptions = {
                 from: "My Ecommerce",
                 to: email,
                 html: `<h1>Confirmación de Compra</h1>
-    <p>Gracias por tu compra, ${firt_name}</p>
+    <p>Gracias por tu compra, ${first_name}</p>
     <p>El número de tu orden es:${ticket}</p>
     <strong>${token}</strong>
     <p>Este código expira en una hora</p>
@@ -30,8 +30,25 @@ class EmailManager {
             console.log("Error al enviar el mail", error)
         }
     }
-    async enviarCorreoRestablecimiento() {
+    async sendRecoveryMail(email, first_name, token) {
+        try {
+            const mailOptions = {
+                from: "My Ecommerce",
+                to: email,
+                subject: "Restablecimiento de Contraseña",
+                html: `<h1>Restablecimiento de Contraseña</h1>
+    <p>Hola ${first_name}</p>
+    <p>Has perdido tu contraseña</p>
+    <p>Te enviaremos este código de confirmación: </p>
+    <strong>${token}</strong>
+    <p>Este código expira en una hora</p>
+    <a href="http://localhost:8080/password">Restablecer Contraseña</a>
+    `
+            }
 
+        } catch (error) {
+            console.log("Error al enviar al correo de restablecimiento", error)
+        }
     }
 }
 export default EmailManager
